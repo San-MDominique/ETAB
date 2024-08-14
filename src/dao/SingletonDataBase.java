4d5e569851f14bf42684df0faada3d37c011c620
San-MDominique <nogboumariedominique@gmail.com>
2024-08-14 12:07:06 +0200

    tree commit Etab

--- src/dao/SingletonDataBase.java
+++ src/dao/SingletonDataBase.java
@@ -14,22 +14,22 @@ public class SingletonDataBase {
     // Instance unique de la connexion
     private static volatile Connection connection = null;
 
-    // Constructeur  pour empêcher l'instanciation directe
+    // Constructeur  instanciation d
     private SingletonDataBase() {
         // Initialisation de la connexion dans le constructeur
         try {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
-            // Gestion de l'exception avec un message d'erreur plus descriptif
+            //  d'erreur plus descriptif
             System.err.println("Erreur lors de la connexion à la base de données: " + e.getMessage());
         }
     }
 
-    // Méthode pour obtenir l'instance unique de la connexion
+    // la connexion
     public static Connection getInstance() {
         if (connection == null) {
             synchronized (SingletonDataBase.class) {
-                // Double vérification pour éviter des problèmes en environnement multithread
+                //
                 if (connection == null) {
                     new SingletonDataBase();
                 }
