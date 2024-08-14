53b86fd63cf0ab40db41263ca932b3dea1a46421
San-MDominique <nogboumariedominique@gmail.com>
2024-08-14 12:04:44 +0200

    two commit Etab

--- src/dao/SingletonDataBase.java
+++ src/dao/SingletonDataBase.java
@@ -5,27 +5,35 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SingletonDataBase {
-    //URL de connexion à la base de données etab_db
-    private final static String URL="jdbc:mysql://localhost:3306/etab_db";
+    // URL de connexion à la base de données
+    private static final String URL = "jdbc:mysql://localhost:3306/etab_db";
     // Nom d'utilisateur
     private static final String USER = "Domi";
     private static final String PASSWORD = "1234";
-    // Déclaration d'un objet connexion qui provient de java.sql.Connection
-    static Connection connection;
 
-    private SingletonDataBase(){
-        try  {
-            // Création d'une nouvelle connexion à la base de données
+    // Instance unique de la connexion
+    private static volatile Connection connection = null;
+
+    // Constructeur  pour empêcher l'instanciation directe
+    private SingletonDataBase() {
+        // Initialisation de la connexion dans le constructeur
+        try {
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
-
         } catch (SQLException e) {
-            //
-            System.out.println(e.getMessage());
+            // Gestion de l'exception avec un message d'erreur plus descriptif
+            System.err.println("Erreur lors de la connexion à la base de données: " + e.getMessage());
         }
     }
-    public static Connection getInstance(){
-        if(connection==null){
-          new SingletonDataBase();
+
+    // Méthode pour obtenir l'instance unique de la connexion
+    public static Connection getInstance() {
+        if (connection == null) {
+            synchronized (SingletonDataBase.class) {
+                // Double vérification pour éviter des problèmes en environnement multithread
+                if (connection == null) {
+                    new SingletonDataBase();
+                }
+            }
         }
         return connection;
     }
